5165b429d8fd5e6e79a37b40fc621fe2474133c8
Othmane Outama <outama.othmane@gmail.com>
2021-06-03 21:05:32 +0100

    [dev] : First commit

---
+++ src/main/java/ma/ac/ensa/ebankingapi/EBankingApiApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.ensa.ebankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankingApiApplication.class, args);
+	}
+
+}
